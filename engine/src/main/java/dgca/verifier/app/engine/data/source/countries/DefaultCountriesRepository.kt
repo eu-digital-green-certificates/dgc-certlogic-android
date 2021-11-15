d72a53363e7f04ba7c3eef09f9971f01e28bb7b8
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-11-15 10:32:48 +0200

    Update

--- engine/src/main/java/dgca/verifier/app/engine/data/source/countries/DefaultCountriesRepository.kt
+++ engine/src/main/java/dgca/verifier/app/engine/data/source/countries/DefaultCountriesRepository.kt
@@ -27,6 +27,28 @@ import dgca.verifier.app.engine.data.source.remote.countries.CountriesRemoteData
 import kotlinx.coroutines.flow.Flow
 import java.util.*
 
+/*-
+ * ---license-start
+ * eu-digital-green-certificates / dgc-certlogic-android
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ *
+ * Created by osarapulov on 25.06.21 9:28
+ */
+
 class DefaultCountriesRepository(
     private val remoteDataSource: CountriesRemoteDataSrouce,
     private val localDataSource: CountriesLocalDataSource
@@ -35,9 +57,7 @@ class DefaultCountriesRepository(
     override suspend fun preLoadCountries(countriesUrl: String) {
         remoteDataSource.getCountries(countriesUrl)
             .map { it.toLowerCase(Locale.ROOT) }
-            .apply {
-                localDataSource.updateCountries(this)
-            }
+            .apply { localDataSource.updateCountries(this) }
     }
 
     override fun getCountries(): Flow<List<String>> {
